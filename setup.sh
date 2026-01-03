b5b61ca5303f9d152364efaff2863332df048704
trunghieu1604 <trhieuforwork@gmail.com>
2026-01-03 15:20:49 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ rm -f $HOME/*.apk
 rm -f $HOME/*.sh
 echo "Clean up old files done."
 
-wget -O $HOME/download.sh "https://raw.githubusercontent.com/trunghieu1604/r1-sh/main/voicebot.sh"
+wget -O $HOME/download.sh "https://raw.githubusercontent.com/trunghieu1604/r1-sh/main/download.sh"
 chmod +x $HOME/download.sh
 chmod +x $HOME/voicebot.sh
 
 echo "[1/2] Chuan bi cai dat..."
-$HOME/download.sh $DOMAIN $VERSION
+$HOME/download.sh
 echo "[2/2] Cai dat Voicebot..."
-$HOME/voicebot.sh $VERSION || true
+$HOME/voicebot.sh || true
 echo "Cai dat hoan tat."
 echo "Doi thiet bi khoi lai xong."
 echo "Vao wifi Phicomm R1, truy cap http://192.168.43.1:8081 de cau hinh Wi-Fi cho thiet bi."
