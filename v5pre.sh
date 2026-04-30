fcfb34f73ed7f3ad9ef92a7cb319f3819cf1092a
trunghieu1604 <trhieuforwork@gmail.com>
2026-04-30 15:56:17 +0700

    Update v5pre.sh

--- v5pre.sh
+++ v5pre.sh
@@ -5,14 +5,14 @@ rm -f $HOME/*.sh
 echo "Clean up old files done."
 
 wget -O $HOME/taive.sh "https://raw.githubusercontent.com/trunghieu1604/r1-sh/main/taive.sh"
-wget -O $HOME/vietbotpre.sh "https://raw.githubusercontent.com/trunghieu1604/r1-sh/main/vietbotpre.sh"
+wget -O $HOME/vietbot.sh "https://raw.githubusercontent.com/trunghieu1604/r1-sh/main/vietbot.sh"
 chmod +x $HOME/taive.sh
-chmod +x $HOME/vietbotpre.sh
+chmod +x $HOME/vietbot.sh
 
 echo "[1/2] Chuan bi cai dat..."
 $HOME/taive.sh
 echo "[2/2] Cai dat Vietbot..."
-$HOME/vietbotpre.sh || true
+$HOME/vietbot.sh || true
 echo "Cai dat hoan tat."
 echo "Doi thiet bi khoi lai xong."
 echo "Vao wifi Phicomm R1, truy cap http://192.168.43.1:8081 de cau hinh Wi-Fi cho thiet bi."
